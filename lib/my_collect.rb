e50e982a43daea890874f9ca2381e2fc47056155
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-10-29 18:55:06 -0600

    commit tree 6e9399cbaa1f2d8e6b378b6df3bd44b574f37807

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+require "pry"
+def my_collect(array)
+    i = 0
+    collection = []
+    if array.length > 0
+        while i < array.length
+            #binding.pry
+            collection << yield(array[i])
+            i += 1
+        end
+        collection
+    end
+end
 
